b9e9701709933a8adf8c9ec59dbe35eda6b7d6e9
brochhay <74852547+Brochhay@users.noreply.github.com>
2025-02-27 10:56:50 +0700

    Update footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -6,26 +6,7 @@ import { Button } from "./button";
 export default function Footer() {
   return (
     <footer>
-      <Gradient className="relative">
-
-            <p className="font-mono text-xs/5 font-semibold uppercase tracking-widest text-gray-500">
-              
-            </p>
-            <p className="mx-auto mt-6 text-3xl text-gray-950 sm:text-5xl tracking-tight font-medium">
-              <br />
-              
-            </p>
-            <p className="mx-auto mt-6 max-w-xs text-sm/6 text-gray-500">
-              
-            </p>
-            <div className="mt-6">
-              <Button className="w-full sm:w-auto" href="/login">
-                
-              </Button>
-            </div>
-          </div>
-        </Container>
-      </Gradient>
+     
     </footer>
   );
 }
